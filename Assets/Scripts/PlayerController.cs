3b8c55b4ab9ab75b6ad1b00ac4d52bb25ab64a89
kienda <83455653+kida31@users.noreply.github.com>
2026-03-26 13:19:29 +0100

    add pickup with count

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,56 @@
+using TMPro;
 using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
-    public float speed = 0;
+
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        
+        SetCountText();
     }
 
-    void OnMove (InputValue movementValue)
+    void OnMove(InputValue movementValue)
     {
-         Vector2 movementVector = movementValue.Get<Vector2>();
-         movementX = movementVector.x;
-         movementY = movementVector.y;
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
-    }     
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 5)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
 }
