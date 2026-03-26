be9735018dd5b0f1efc7ae4ff1c8a1cee8a29dc8
kienda <83455653+kida31@users.noreply.github.com>
2026-03-26 11:51:25 +0100

    Add "moving the player"

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove (InputValue movementValue)
+    {
+         Vector2 movementVector = movementValue.Get<Vector2>();
+         movementX = movementVector.x;
+         movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }     
+}
